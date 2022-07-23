107c0f5aec0bdbd379d4b9fd1666a788eaeadada
Smith <william.smith@bankerslife.com>
2022-07-23 16:42:15 -0400

    added home page

---
+++ views/home.jsx
@@ -0,0 +1,22 @@
+const React = require('react')
+const Def = require('./default')
+
+
+function home () {
+    return (
+      <Def>
+          <main>
+              <h1>HOME</h1>
+          </main>
+      </Def>
+    )
+  }
+  
+
+
+
+
+
+
+
+module.exports = home
